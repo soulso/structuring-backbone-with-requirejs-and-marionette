304b202d35cdf99467691c35f6d7b4a768f67685
David Sulc <davidsulc@gmail.com>
2013-07-06 16:16:15 +0200

    load list views with require

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -1,16 +1,14 @@
-define(["apps/../app", "entities/contact"], function(ContactManager){
+define(["apps/../app", "entities/contact", "apps/contacts/list/list_view"], function(ContactManager){
   ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
     List.Controller = {
       listContacts: function(){
         var contacts = ContactManager.request("contact:entities");
 
-        console.log("contacts: ", contacts);
+        var contactsListView = new List.Contacts({
+          collection: contacts
+        });
 
-  //       var contactsListView = new List.Contacts({
-  //         collection: contacts
-  //       });
-  //
-  //       ContactManager.mainRegion.show(contactsListView);
+        ContactManager.mainRegion.show(contactsListView);
       }
     }
   });
--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,11 +1,15 @@
-ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
-  List.Contact = Backbone.Marionette.ItemView.extend({
-    tagName: "li",
-    template: "#contact-list-item"
+define(["apps/../app"], function(ContactManager){
+  ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
+    List.Contact = Backbone.Marionette.ItemView.extend({
+      tagName: "li",
+      template: "#contact-list-item"
+    });
+
+    List.Contacts = Backbone.Marionette.CollectionView.extend({
+      tagName: "ul",
+      itemView: List.Contact
+    });
   });
 
-  List.Contacts = Backbone.Marionette.CollectionView.extend({
-    tagName: "ul",
-    itemView: List.Contact
-  });
+  return;
 });
